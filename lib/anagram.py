7b5be56853df2c05ff90f97944c21ebcd2caefbb
Arnold-In-Tech <g16a7782@gmail.com>
2024-03-02 07:16:52 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+
+# pseudocode
+"""
+- obtain each element from match list
+- check if elent when sorted equal word when sorted
+"""
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        matches = [ele for ele in anagram_list if sorted(list(ele)) == sorted(list(self.word))]
+        return matches
+
+
+# test code                 
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])        
